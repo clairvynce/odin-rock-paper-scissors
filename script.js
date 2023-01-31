3148c1a742dc0b8e3339621453bf07a36ad2555c
clairvynce <clairvynce@gmail.com>
2023-01-31 18:12:37 +0300

    refactor: change game logic

--- script.js
+++ script.js
@@ -3,42 +3,51 @@
 function game() {
   let playerScore = 0;
   let computerScore = 0;
+
   for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Choose your weapon").toLowerCase();
-    if (playRound(playerSelection, getComputerChoice()) === "You win!") {
-      console.log("You win");
+    const roundWinner = playRound(getPlayerChoice(), getComputerChoice());
+
+    if (roundWinner == "player") {
       playerScore++;
-    } else if (
-      playRound(playerSelection, getComputerChoice()) === "Computer wins!"
-    ) {
-      console.log("You lose");
+      console.log(`Player wins! ${playerScore} : ${computerScore}`);
+    } else if (roundWinner === "computer") {
       computerScore++;
+      console.log(`Computer wins! ${playerScore} : ${computerScore}`);
     } else {
-      console.log("Draw");
-      i--;
+      console.log(`It's a tie! ${playerScore} : ${computerScore}`);
     }
   }
 
-  alert(playerScore > computerScore ? "Player wins!" : "Computer wins!");
+  if (playerScore > computerScore) {
+    alert(`You win! ${playerScore} : ${computerScore}`);
+  } else if (playerScore === computerScore) {
+    alert(`Draw! ${playerScore} : ${computerScore}`);
+  } else {
+    alert(`You lose! ${playerScore} : ${computerScore}`);
+  }
+}
+
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === "rock" && computerChoice === "scissors") {
+    return "player";
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    return "player";
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    return "player";
+  } else if (playerChoice === computerChoice) {
+    return "tie";
+  } else {
+    return "computer";
+  }
+}
+
+function getPlayerChoice() {
+  return prompt("Choose your weapon").toLowerCase();
 }
 
 function getComputerChoice() {
-  let variants = ["rock", "paper", "scissors"];
-  return variants[Math.floor(Math.random() * 3)];
-}
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win!";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win!";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win!";
-  } else if (playerSelection === computerSelection) {
-    return "It's a tie!";
-  } else {
-    return "Computer wins!";
-  }
+  const variants = ["rock", "paper", "scissors"];
+  return variants[Math.floor(Math.random() * variants.length)];
 }
 
 game();
